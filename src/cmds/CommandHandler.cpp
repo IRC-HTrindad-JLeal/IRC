6b3069ce2a88ef9295d9cced366c18f61efb4a03
Matheus <matheus.buss.0000@gmail.com>
2026-06-19 00:10:48 +0100

    first build of CommandHandler

---
+++ src/cmds/CommandHandler.cpp
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   CommandHandler.cpp                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mely-pan <mely-pan@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/18 16:55:39 by mely-pan          #+#    #+#             */
+/*   Updated: 2026/06/19 00:08:42 by mely-pan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <../../incs/CommandHandler.h>
+
+void	CommandHandler::CommandHandler()
+{
+	inItHandlers();
+}
+
+void	CommandHandler::inItHandlers()
+{
+	_handlers["PASS"] = &CommandHandler::pass;
+	_handlers["NICK"] = &CommandHandler::nick;
+	_handlers["USER"] = &CommandHandler::user;
+	_handlers["CAP"] = &CommandHandler::cap;
+	_handlers["PING"] = &CommandHandler::ping;
+	_handlers["PONG"] = &CommandHandler::pong;
+	_handlers["QUIT"] = &CommandHandler::quit;
+	_handlers["JOIN"] = &CommandHandler::join;
+	_handlers["PRIVMSG"] = &CommandHandler::privmsg;
+	_handlers["MODE"] = &CommandHandler::mode;
+	_handlers["TOPIC"] = &CommandHandler::topic;
+	_handlers["INVITE"] = &CommandHandler::invite;
+	_handlers["KICK"] = &CommandHandler::kick;
+}
+
+bool	CommandHAndler::requiresAuth(const std::string &cmd) const
+{
+	return (
+		cmd != "PASS" &&
+		cmd != "NICK" &&
+		cmd != "USER" &&
+		cmd != "PING" &&
+		cmd != "PONG" &&
+		cmd != "CAP" &&
+		cmd != "QUIT"
+	);
+}
+
+bool	CommandHandler::execute(Server &server, Client &client, const Message &msg)
+{
+	const	std::string &cmd = msg.getCommand();
+
+	return dispatch(cmd, server, client, msg);
+}
+
+bool	CommandHandler::dispatch(const std::string &cmd, Server&Server, Client &client, const Message &msg)
+{
+	if (requiresAuth(cmd) && !client.isRegistered())
+	{
+		Server.sendToClient(client, ":server 451 :You have not registered");
+		return true;
+	}
+	std::map<std::string, CommandFt>::iterator it = _handlers.find(cmd);
+
+	if (it == _handlers.end())
+	{
+		Server.sendToClient(client, ":server 421 :Uknown command");
+		return true;
+	}
+	return (this->*(it->second))(server, client, msg);
+}
+
